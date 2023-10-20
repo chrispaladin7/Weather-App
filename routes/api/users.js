5fb8ab75a9e896dcde732bd9491c4c4ebdd6681a
chrispaladin7 <chrismartey@yahoo.com>
2023-10-20 16:48:37 -0400

    Cleans up

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,6 +10,6 @@ router.post('/', usersCtrl.create);
 // POST /api/users/login
 router.post('/login', usersCtrl.login);
 // GET /api/users/check-token
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+// router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
