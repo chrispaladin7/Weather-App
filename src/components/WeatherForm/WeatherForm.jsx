cc3900e026e9f7b41542fc5e11b4ffde1d6c42e8
chrispaladin7 <chrismartey@yahoo.com>
2023-10-23 13:40:33 -0400

    Adds Weather card template

--- src/components/WeatherForm/WeatherForm.jsx
+++ src/components/WeatherForm/WeatherForm.jsx
@@ -1,3 +1,43 @@
-export default function WeatherForm(){
-    return <h1> Weather Form</h1>;
+import './WeatherForm.css'
+import searchIcon from "../../Assets/search.png"
+import cloudIcon from "../../Assets/cloud.png"
+import drizzleIcon from "../../Assets/drizzle.png"
+import humidityIcon from "../../Assets/humidity.png"
+import rainIcon from "../../Assets/rain.png"
+import snowIcon from "../../Assets/snow.png"
+import windIcon from "../../Assets/wind.png"
+import clearIcon from "../../Assets/clear.png"
+
+export default function WeatherForm() {
+    return (
+        <div className='container'>
+            <div className='top-bar'>
+                <input type="text" className="cityInput" placeholder='Search...'/>
+                <div className="search-icon">
+                    <img src={searchIcon} alt="" />
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={clearIcon} alt="" />
+            </div>
+            <div className="weather-temp">24°F</div>
+            <div className="weather-location">New York</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidityIcon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">75%</div>
+                        <div className="humidity-text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={windIcon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="windspeed-score">18 km/h</div>
+                        <div className="windspeed-text">Wide Speed</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/pages/WeatherPage/WeatherPage.jsx
+++ src/pages/WeatherPage/WeatherPage.jsx
@@ -2,7 +2,6 @@ import WeatherForm from "../../components/WeatherForm/WeatherForm"
 export default function WeatherPage(){
     return (
       <div>
-        <h2>Weather Page</h2>
         <WeatherForm/>
       </div>  
     );
