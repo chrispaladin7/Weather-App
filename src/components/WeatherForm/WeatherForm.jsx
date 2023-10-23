edd6642cdc200df35c562897d09ddfee5860267e
chrispaladin7 <chrismartey@yahoo.com>
2023-10-23 16:33:51 -0400

    implements Weather Search card

--- src/components/WeatherForm/WeatherForm.jsx
+++ src/components/WeatherForm/WeatherForm.jsx
@@ -1,40 +1,110 @@
-import './WeatherForm.css'
-import searchIcon from "../../Assets/search.png"
-import cloudIcon from "../../Assets/cloud.png"
-import drizzleIcon from "../../Assets/drizzle.png"
-import humidityIcon from "../../Assets/humidity.png"
-import rainIcon from "../../Assets/rain.png"
-import snowIcon from "../../Assets/snow.png"
-import windIcon from "../../Assets/wind.png"
-import clearIcon from "../../Assets/clear.png"
+import './WeatherForm.css';
+import searchIcon from '../../Assets/search.png';
+import cloudIcon from '../../Assets/cloud.png';
+import drizzleIcon from '../../Assets/drizzle.png';
+import humidityIcon from '../../Assets/humidity.png';
+import rainIcon from '../../Assets/rain.png';
+import snowIcon from '../../Assets/snow.png';
+import windIcon from '../../Assets/wind.png';
+import clearIcon from '../../Assets/clear.png';
+import { useState } from 'react';
 
 export default function WeatherForm() {
+    const [searchText, setSearchText] = useState('');
+    const [wIcon, setWIcon] = useState(clearIcon);
+
+    const api_key = '4dc891ac30b0d1c7e1d199a12e89c137';
+
+    async function fetchWeather() {
+        const cityLocation = document.getElementsByClassName('cityInput');
+        if (cityLocation[0].value === '') return 0;
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityLocation[0].value}&units=imperial&appid=${api_key}`;
+
+        const response = await fetch(URL);
+        const data = await response.json();
+        const temperature = document.getElementsByClassName('weather-temp');
+        const humidity = document.getElementsByClassName('humidity-percent');
+        const wind = document.getElementsByClassName('windspeed-score');
+        const location = document.getElementsByClassName('weather-location');
+        const longitude = 0;
+        const latitude = 0;
+
+        temperature[0].innerText = Math.floor(data.main.temp) + ' °F';
+        humidity[0].innerText = data.main.humidity + ' %';
+        wind[0].innerText = data.wind.speed + ' km/h';
+        location[0].innerText = data.name;
+
+        const iconCode = data.weather[0].icon;
+        setWIcon(mapWeatherIcon(iconCode));
+    }
+
+    function handleSearch() {
+        fetchWeather();
+        setSearchText('');
+    }
+
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setSearchText(value);
+    }
+
+    const mapWeatherIcon = (iconCode) => {
+        const iconMappings = {
+            '01d': 'https://openweathermap.org/img/wn/01d@2x.png',
+            '01n': 'https://openweathermap.org/img/wn/01n@2x.png',
+            '02d': 'https://openweathermap.org/img/wn/02d@2x.png',
+            '02n': 'https://openweathermap.org/img/wn/02n@2x.png',
+            '03d': 'https://openweathermap.org/img/wn/03d@2x.png',
+            '03n': 'https://openweathermap.org/img/wn/03n@2x.png',
+            '04d': 'https://openweathermap.org/img/wn/04d@2x.png',
+            '04n': 'https://openweathermap.org/img/wn/04n@2x.png',
+            '09d': 'https://openweathermap.org/img/wn/09d@2x.png',
+            '09n': 'https://openweathermap.org/img/wn/09n@2x.png',
+            '10d': 'https://openweathermap.org/img/wn/10d@2x.png',
+            '10n': 'https://openweathermap.org/img/wn/10n@2x.png',
+            '11d': 'https://openweathermap.org/img/wn/11d@2x.png',
+            '11n': 'https://openweathermap.org/img/wn/11n@2x.png',
+            '13d': 'https://openweathermap.org/img/wn/13d@2x.png',
+            '13n': 'https://openweathermap.org/img/wn/13n@2x.png',
+            '50d': 'https://openweathermap.org/img/wn/50d@2x.png',
+            '50n': 'https://openweathermap.org/img/wn/50n@2x.png',
+        };
+
+        return iconMappings[iconCode] || clearIcon;
+    };
+
     return (
-        <div className='container'>
-            <div className='top-bar'>
-                <input type="text" className="cityInput" placeholder='Search...'/>
-                <div className="search-icon">
+        <div className="container">
+            <div className="top-bar">
+                <input
+                    type="text"
+                    className="cityInput"
+                    placeholder="Search..."
+                    value={searchText}
+                    onChange={handleChange}
+                />
+                <div className="search-icon" onClick={handleSearch}>
                     <img src={searchIcon} alt="" />
                 </div>
             </div>
             <div className="weather-image">
-                <img src={clearIcon} alt="" />
+                <img src={wIcon} alt="" />
             </div>
-            <div className="weather-temp">24°F</div>
-            <div className="weather-location">New York</div>
+            <div className="weather-temp">-</div>
+            <div className="weather-location">- -</div>
             <div className="data-container">
                 <div className="element">
                     <img src={humidityIcon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">75%</div>
+                        <div className="humidity-percent">-</div>
                         <div className="humidity-text">Humidity</div>
                     </div>
                 </div>
                 <div className="element">
                     <img src={windIcon} alt="" className="icon" />
                     <div className="data">
-                        <div className="windspeed-score">18 km/h</div>
-                        <div className="windspeed-text">Wide Speed</div>
+                        <div className="windspeed-score">-</div>
+                        <div className="windspeed-text">Wind Speed</div>
                     </div>
                 </div>
             </div>
