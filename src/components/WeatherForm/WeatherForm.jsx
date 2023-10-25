4782f2d56985cfc6de4d69a5031a8e6e31df997d
chrispaladin7 <chrismartey@yahoo.com>
2023-10-25 16:57:06 -0400

    refactors Weather Search component

--- src/components/WeatherForm/WeatherForm.jsx
+++ src/components/WeatherForm/WeatherForm.jsx
@@ -7,42 +7,10 @@ import windIcon from '../../Assets/wind.png';
 import clearIcon from '../../Assets/clear.png';
 import WeatherDetailModal from '../WeatherDetail/WeatherDetailModal';
 
-export default function WeatherForm({ handleSearch, weatherData,location,description,temperature,humidity,windspeed }) {
+export default function WeatherForm({ handleSearch, weatherData,location,description,temperature,humidity,windspeed,animationImage,weatherIcon }) {
     const [searchText, setSearchText] = useState('');
-    const [wIcon, setWIcon] = useState(clearIcon);
-    const [animationImage, setAnimationImage] = useState('');
+    // const [animationImage, setAnimationImage] = useState('');
     const [openWeatherModal, setWeatherModal] = useState(false);
-    // const [ description, temp, name, humidity, speed ] = useState(weatherData);
-    
-
-    const mapWeatherIcon = (iconCode) => {
-        const iconMappings = {
-            '01d': 'https://openweathermap.org/img/wn/01d@2x.png',
-            '01n': 'https://openweathermap.org/img/wn/01n@2x.png',
-            '02d': 'https://openweathermap.org/img/wn/02d@2x.png',
-            '02n': 'https://openweathermap.org/img/wn/02n@2x.png',
-            '03d': 'https://openweathermap.org/img/wn/03d@2x.png',
-            '03n': 'https://openweathermap.org/img/wn/03n@2x.png',
-            '04d': 'https://openweathermap.org/img/wn/04d@2x.png',
-            '04n': 'https://openweathermap.org/img/wn/04n@2x.png',
-            '09d': 'https://openweathermap.org/img/wn/09d@2x.png',
-            '09n': 'https://openweathermap.org/img/wn/09n@2x.png',
-            '10d': 'https://openweathermap.org/img/wn/10d@2x.png',
-            '10n': 'https://openweathermap.org/img/wn/10n@2x.png',
-            '11d': 'https://openweathermap.org/img/wn/11d@2x.png',
-            '11n': 'https://openweathermap.org/img/wn/11n@2x.png',
-            '13d': 'https://openweathermap.org/img/wn/13d@2x.png',
-            '13n': 'https://openweathermap.org/img/wn/13n@2x.png',
-            '50d': 'https://openweathermap.org/img/wn/50d@2x.png',
-            '50n': 'https://openweathermap.org/img/wn/50n@2x.png',
-        };
-
-        return iconMappings[iconCode] || clearIcon;
-    };
-
-
-
-
 
     function handleChange(evt) {
         const value = evt.target.value;
@@ -74,23 +42,23 @@ export default function WeatherForm({ handleSearch, weatherData,location,descrip
             </div>
 
             <div className={`weather-image ${animationImage}`}>
-                <img src={wIcon} alt="" />
+                <img src={weatherIcon} alt="" />
             </div>
-            <div className="weather-description"></div>
-            <div className="weather-temp"></div>
+            <div className="weather-description">{description}</div>
+            <div className="weather-temp">{temperature}</div>
             <div className="weather-location">{location}</div>
             <div className="data-container">
                 <div className="element">
                     <img src={humidityIcon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">-</div>
+                        <div className="humidity-percent">{humidity}</div>
                         <div className="humidity-text">Humidity</div>
                     </div>
                 </div>
                 <div className="element">
                     <img src={windIcon} alt="" className="icon" />
                     <div className="data">
-                        <div className="windspeed-score">-</div>
+                        <div className="windspeed-score">{windspeed}</div>
                         <div className="windspeed-text">Wind Speed</div>
                     </div>
                 </div>
--- src/pages/WeatherPage/WeatherPage.jsx
+++ src/pages/WeatherPage/WeatherPage.jsx
@@ -6,24 +6,54 @@ import humidityIcon from '../../Assets/humidity.png';
 import windIcon from '../../Assets/wind.png';
 import clearIcon from '../../Assets/clear.png';
 
-export default function WeatherPage( { setWeatherData,weatherData } ){
+export default function WeatherPage({ setWeatherData, weatherData }) {
     const [description, setDescription] = useState('');
     const [temperature, setTemperature] = useState('');
     const [location, setLocation] = useState('');
     const [humidity, setHumidity] = useState('');
     const [windspeed, setWindSpeed] = useState('');
+    const [wIcon, setWIcon] = useState(clearIcon);
+    const [animationImage, setAnimationImage] = useState('');
+    const [searchText, setSearchText] = useState('');
+    
+    const mapWeatherIcon = (iconCode) => {
+        const iconMappings = {
+            '01d': 'https://openweathermap.org/img/wn/01d@2x.png',
+            '01n': 'https://openweathermap.org/img/wn/01n@2x.png',
+            '02d': 'https://openweathermap.org/img/wn/02d@2x.png',
+            '02n': 'https://openweathermap.org/img/wn/02n@2x.png',
+            '03d': 'https://openweathermap.org/img/wn/03d@2x.png',
+            '03n': 'https://openweathermap.org/img/wn/03n@2x.png',
+            '04d': 'https://openweathermap.org/img/wn/04d@2x.png',
+            '04n': 'https://openweathermap.org/img/wn/04n@2x.png',
+            '09d': 'https://openweathermap.org/img/wn/09d@2x.png',
+            '09n': 'https://openweathermap.org/img/wn/09n@2x.png',
+            '10d': 'https://openweathermap.org/img/wn/10d@2x.png',
+            '10n': 'https://openweathermap.org/img/wn/10n@2x.png',
+            '11d': 'https://openweathermap.org/img/wn/11d@2x.png',
+            '11n': 'https://openweathermap.org/img/wn/11n@2x.png',
+            '13d': 'https://openweathermap.org/img/wn/13d@2x.png',
+            '13n': 'https://openweathermap.org/img/wn/13n@2x.png',
+            '50d': 'https://openweathermap.org/img/wn/50d@2x.png',
+            '50n': 'https://openweathermap.org/img/wn/50n@2x.png',
+        };
+
+        return iconMappings[iconCode] || clearIcon;
+    };
 
     async function handleSearch(searchText) {
         try {
-            // setAnimationImage('');
+            setAnimationImage('');
             const data = await fetchWeather(searchText);
             setWeatherData(data);
-            setLocation(data.name);
-            setHumidity(data.main.humidity);
-            // setSearchText('');
-            // setWIcon(mapWeatherIcon(data?.weather[0]?.icon));
-
-            // setAnimationImage('animate__animated animate__fadeInUp');
+            setLocation(`${data.name}`.toUpperCase());
+            setHumidity(`${data.main.humidity} %`);
+            setDescription(data.weather[0].main);
+            setTemperature(`${Math.floor(data.main.temp)} °F`);
+            setWindSpeed(`${data.wind.speed} km/h`);
+            setSearchText('');
+            setWIcon(mapWeatherIcon(data?.weather[0]?.icon));
+            setAnimationImage('animate__animated animate__fadeInUp');
 
 
         } catch (error) {
@@ -32,16 +62,18 @@ export default function WeatherPage( { setWeatherData,weatherData } ){
     }
 
     return (
-      <div>
-        <WeatherForm 
-        handleSearch={handleSearch}
-        weatherData={weatherData}
-        location={location}
-        description={description}
-        temperature={temperature}
-        humidity={humidity}
-        windspeed={windspeed}
-        />
-      </div>  
+        <div>
+            <WeatherForm
+                handleSearch={handleSearch}
+                weatherData={weatherData}
+                location={location}
+                description={description}
+                temperature={temperature}
+                humidity={humidity}
+                windspeed={windspeed}
+                animationImage={animationImage}
+                weatherIcon={wIcon}
+            />
+        </div>
     );
 }
